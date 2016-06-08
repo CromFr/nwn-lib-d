e34a424db883882bb974d8c4155464f4ffb57f4f
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2016-06-08 15:37:44 +0200

    tmp fix: doc build (using my fork of scod)

--- build-doc.sh
+++ build-doc.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-dub fetch scod
+# dub fetch -q scod
+if [ ! -d ../scod ]; then
+	git clone https://github.com/CromFr/scod.git ../scod
+	dub add-local ../scod
+fi
 
 # Gen doc
 DFLAGS='-c -o- -Df__dummy.html -Xfdocs.json' dub build
-dub run scod -- filter --min-protection=Protected --only-documented docs.json
-dub run scod -- generate-html --navigation-type=ModuleTree docs.json docs
+dub run -q scod -- filter --min-protection=Protected --only-documented docs.json
+dub run -q scod -- generate-html --navigation-type=ModuleTree docs.json docs
 
 # Install static files
 pkg_path=$(dub list | sed -n 's|.*scod.*: ||p')
