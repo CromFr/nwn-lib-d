f94d190479466e00d0b73eaa0534bd8e7fd6a227
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2016-06-23 15:34:21 +0200

    appendField ref removed

--- source/nwn/gff.d
+++ source/nwn/gff.d
@@ -82,6 +82,7 @@ struct GffNode{
 		label = lbl;
 	}
 
+	/// Duplicate GffNode data
 	GffNode dup() const
 	{
 		GffNode ret;
@@ -467,7 +468,7 @@ struct GffNode{
 	}
 
 	/// Adds a GffNode to a $(D GffNode.Struct), using its label as key
-	GffNode* appendField(ref GffNode field){
+	GffNode* appendField(GffNode field){
 		assert(type == GffType.Struct, "GffNode must be a struct");
 		assert(field.type != GffType.Invalid, "Cannot append invalid GffNode");
 
