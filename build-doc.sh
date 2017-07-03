eec613e71b03bd3609af8b7f050133f9972a24a0
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2017-07-03 18:42:12 +0200

    document everything !

--- build-doc.sh
+++ build-doc.sh
@@ -4,18 +4,18 @@ set -ev
 # dub fetch -q scod
 if [ ! -d scod ]; then
 	git clone https://github.com/MartinNowak/scod.git scod
-	
+
 	cd scod
 	sed -i 's/"vibe-d":\s"0\.7\.30"/"vibe-d": "0.7.31"/' dub.selections.json
 	echo 'versions "VibeNoSSL"' >> dub.sdl
-	
+
 	dub build
 	cd ..
 fi
 
 # Gen doc
 DFLAGS='-c -o- -Df__dummy.html -Xfdocs.json' dub build
-scod/scod -- filter --min-protection=Protected --only-documented docs.json
+scod/scod -- filter --min-protection=Protected docs.json
 scod/scod -- generate-html --navigation-type=ModuleTree docs.json docs
 
 # Install static files
