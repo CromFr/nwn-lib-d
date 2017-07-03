16030daebbb565f79188d4662ec61b4fcc56a07c
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2017-07-03 17:39:23 +0200

    fix: documentation generation

--- build-doc.sh
+++ build-doc.sh
@@ -4,14 +4,21 @@ set -ev
 # dub fetch -q scod
 if [ ! -d scod ]; then
 	git clone https://github.com/MartinNowak/scod.git scod
-	dub add-local scod
+	
+	cd scod
+	sed -i 's/"vibe-d":\s"0\.7\.30"/"vibe-d": "0.7.31"/' dub.selections.json
+	echo 'versions "VibeNoSSL"' >> dub.sdl
+	
+	dub build
+	cd ..
 fi
 
 # Gen doc
-#DFLAGS='-c -o- -Df__dummy.html -Xfdocs.json' dub build
-#dub run -q scod -- filter --min-protection=Protected --only-documented docs.json
-#dub run -q scod -- generate-html --navigation-type=ModuleTree docs.json docs
+DFLAGS='-c -o- -Df__dummy.html -Xfdocs.json' dub build
+scod/scod -- filter --min-protection=Protected --only-documented docs.json
+scod/scod -- generate-html --navigation-type=ModuleTree docs.json docs
 
 # Install static files
-pkg_path=$(dub list | sed -n 's|.*scod.*: ||p')
-rsync -ru "$pkg_path"public/ docs/
+#pkg_path=$(dub list | sed -n 's|.*scod.*: ||p')
+pkg_path=
+rsync -ru scod/public/ docs/
