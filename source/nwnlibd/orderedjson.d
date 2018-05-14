afdd2fab071a24bf5407c67e9be515be69b2d266
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2018-05-15 01:08:39 +0200

    Remove deprecations

--- source/nwnlibd/orderedjson.d
+++ source/nwnlibd/orderedjson.d
@@ -102,7 +102,7 @@ struct JSONValue
 {
     public string[] objectKeyOrder;
 
-    import std.exception : enforceEx, enforce;
+    import std.exception : enforce;
 
     union Store
     {
@@ -473,7 +473,7 @@ struct JSONValue
     ref inout(JSONValue) opIndex(size_t i) inout pure @safe
     {
         auto a = this.arrayNoRef;
-        enforceEx!JSONException(i < a.length,
+        enforce!JSONException(i < a.length,
                                 "JSONValue array index is out of range");
         return a[i];
     }
@@ -513,7 +513,7 @@ struct JSONValue
      */
     void opIndexAssign(T)(auto ref T value, string key) pure
     {
-        enforceEx!JSONException(type == JSON_TYPE.OBJECT || type == JSON_TYPE.NULL,
+        enforce!JSONException(type == JSON_TYPE.OBJECT || type == JSON_TYPE.NULL,
                                 "JSONValue must be object or null");
         JSONValue[string] aa = null;
         if (type == JSON_TYPE.OBJECT)
@@ -536,7 +536,7 @@ struct JSONValue
     void opIndexAssign(T)(T arg, size_t i) pure
     {
         auto a = this.arrayNoRef;
-        enforceEx!JSONException(i < a.length,
+        enforce!JSONException(i < a.length,
                                 "JSONValue array index is out of range");
         a[i] = arg;
         this.array = a;
