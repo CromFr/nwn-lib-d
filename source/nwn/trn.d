1db8fab88a88beef9d75752c0331b631ee7120a4
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2018-06-17 21:25:37 +0200

    Line / triangle collision detection

--- source/nwn/trn.d
+++ source/nwn/trn.d
@@ -7,6 +7,7 @@ import std.conv: to;
 import std.traits;
 import std.exception: enforce;
 import std.algorithm;
+import std.math;
 import std.array: array;
 import nwnlibd.parseutils;
 import nwnlibd.geometry;
@@ -2006,4 +2007,77 @@ struct GenericASWMMesh {
 		}
 	}
 
+
+	auto findTrianglesIntersecting(vec2f[2] line) const {
+		struct TriCut{
+			uint32_t triangle;
+			ubyte[] cutEdges;
+		}
+		TriCut[] ret;
+
+
+		auto lineVec = vec2f(line[1] - line[0]).normalized;
+
+		auto lineVecPer = vec2f(lineVec[1], -lineVec[0]);
+		auto lineDist = line[0].dot(lineVecPer);
+
+		foreach(uint32_t i, ref t ; triangles){
+
+			vec2f[3] triVertices = t.vertices[]
+				.map!(a => vec2f(vertices[a].v[0 .. 2]))
+				.array[0 .. 3];
+
+			// Check if the triangle is aligned with the cur line
+			float[3] ndot;
+			foreach(j ; 0 .. 3){
+				// we calculate the distance between the cutting line and each
+				// point of the triangle.
+				// If the distance will be >0 or <0 depending on which side of
+				// the line is the point.
+				ndot[j] = vec2f(triVertices[j]).dot(lineVecPer) - lineDist;
+			}
+
+			ubyte[] cutEdges;// will contain the list of edges cut by an infinite line
+			static foreach(j ; 0 .. 3){
+				// ndot[j] * ndot[(j + 1) % 3] is <0 if the two vertices of
+				// a triangle edge are on each side of the cutting line
+				if(ndot[j] * ndot[(j + 1) % 3] < 0)
+					cutEdges ~= j;
+			}
+
+			if(cutEdges.length == 0)
+				continue; // The cutting line does not cross any triangle edge
+
+			// TODO: What happens if the cutting line goes exactly through a vertex, and cut only one edge
+			assert(cutEdges.length <= 2,
+				"You succeeded at cutting "~cutEdges.length.to!string~" edges of a triangle with an infinite line");
+
+			auto cw = isTriangleClockwise(triVertices);
+
+			// bool[edge 0/1][line start/end]
+			bool[2][2] edgeCw;
+			foreach(iLinePoint, linePoint ; line){
+				foreach(j, edge ; cutEdges){
+					edgeCw[j][iLinePoint] = isTriangleClockwise([triVertices[edge], triVertices[(edge + 1) % 3], linePoint]);
+				}
+			}
+
+			auto triCut = TriCut(i, []);
+
+			foreach(j, edge ; cutEdges){
+				// value == cw <=> point is inside of the triangle edge
+				if((edgeCw[j][0] == cw && edgeCw[j][1] != cw)
+				|| (edgeCw[j][0] != cw && edgeCw[j][1] == cw))
+					triCut.cutEdges ~= edge;
+			}
+
+			if(triCut.cutEdges.length == 0)
+				continue;
+
+
+			ret ~= triCut;
+		}
+		return ret;
+
+	}
 }
