b70a7383fe822f89b64522c65da8b2d6dbf06ca8
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2018-07-13 11:31:29 +0200

    Try going back to dmd 2.080.1

--- .travis/build-win.sh
+++ .travis/build-win.sh
@@ -12,14 +12,15 @@ if [[ "$1" == "before_install" ]]; then
 	sudo apt-key add Release.key
 	sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 
-	sudo apt-get update
-	sudo apt-get install --install-recommends -y winehq-stable p7zip
+	sudo apt-get update -qq
+	sudo apt-get install --install-recommends -qq -y winehq-stable p7zip
 
 	wineboot
 
 	INSTALL_DIR=$WINEPREFIX/drive_c/dmd
 
-	DMD_VERSION=`dmd --version | grep -P -o "2\.\d{3}\.\d"`
+	# DMD_VERSION=`dmd --version | grep -P -o "2\.\d{3}\.\d"`
+	DMD_VERSION="2.080.1"
 	wget http://downloads.dlang.org/releases/2.x/${DMD_VERSION}/dmd.${DMD_VERSION}.windows.7z -O /tmp/dmd.7z
 	7zr x -o$INSTALL_DIR /tmp/dmd.7z
 
