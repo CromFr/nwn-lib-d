c725927801e584a8fe42378ca7721d62a9930f78
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2020-10-05 22:52:15 +0200

    more bench info

--- tools/benchmark.d
+++ tools/benchmark.d
@@ -1,34 +1,50 @@
 import std.stdio;
 import std.datetime.stopwatch;
 
-import nwn.gff;
 
 void main(){
 
-	StopWatch sw;
-	Duration min = Duration.max;
-	foreach(i ; 0 .. 100){
-		sw.reset();
-		sw.start();
-		new Gff(cast(immutable ubyte[])import("krogar.bic"));
-		sw.stop();
-		if(sw.peek() < min)
-			min = sw.peek();
-	}
-	stderr.writeln("GFF Parsing: ", min.total!"usecs" / 1000.0, "ms");
+	{
+		import nwn.gff;
+		StopWatch sw;
+		Duration min = Duration.max;
+		foreach(i ; 0 .. 100){
+			sw.reset();
+			sw.start();
+			new Gff(cast(immutable ubyte[])import("krogar.bic"));
+			sw.stop();
+			if(sw.peek() < min)
+				min = sw.peek();
+		}
+		stderr.writeln("nwn.gff.Gff Parsing: ", min.total!"usecs" / 1000.0, "ms");
 
-	auto gff = new Gff(cast(immutable ubyte[])import("krogar.bic"));
-	min = Duration.max;
-	sw.reset();
-	foreach(i ; 0 .. 100){
+		auto gff = new Gff(cast(immutable ubyte[])import("krogar.bic"));
+		min = Duration.max;
 		sw.reset();
-		sw.start();
-		gff.serialize();
+		foreach(i ; 0 .. 100){
+			sw.reset();
+			sw.start();
+			gff.serialize();
+			sw.stop();
+			if(sw.peek() < min)
+				min = sw.peek();
+		}
 		sw.stop();
-		if(sw.peek() < min)
-			min = sw.peek();
+		stderr.writeln("nwn.gff.Gff Serialization: ", min.total!"usecs" / 1000.0, "ms");
 	}
-	sw.stop();
-	stderr.writeln("GFF Serialization: ", min.total!"usecs" / 1000.0, "ms");
+	{
+		import nwn.fastgff;
+		StopWatch sw;
+		Duration min = Duration.max;
+		foreach(i ; 0 .. 100){
+			sw.reset();
+			sw.start();
+			new FastGff(cast(immutable ubyte[])import("krogar.bic"));
+			sw.stop();
+			if(sw.peek() < min)
+				min = sw.peek();
+		}
+		stderr.writeln("nwn.fastgff.FastGff Parsing: ", min.total!"usecs" / 1000.0, "ms");
+	}
 
 }
